9136cd9218003f0bf783ce63b4466e466bc983c9
portfolio-y0711 <portfolio-y0711@gmail.com>
2021-03-18 14:41:07 +0900

    modify codes to load file when the database does not work

--- subprojects/api/src/server/controllers/index.ts
+++ subprojects/api/src/server/controllers/index.ts
@@ -1,7 +1,7 @@
-import { pathCacheService } from "../../service"
+import { pathService, pathCacheService } from "../../service"
 import { asyncHandler } from "./handler.async"
 
-const createPathController = ((service: any) => {
+const createPathController = (({ service, cacheService }: { service: any, cacheService: any}) => {
     // eslint-disable-next-line @typescript-eslint/require-await
     const getPathHandler = async(httpRequest: any) => {
         const { id } = httpRequest.params
@@ -10,7 +10,10 @@ const createPathController = ((service: any) => {
         try {
             result = await service.getPath(id)
         } catch(e) {
-            console.log(e)
+            console.error(e)
+            console.error('--------------------------------------------------------------')
+            console.error('[!!!] production database is not running!, using cached filedb')
+            result = await cacheService.getPath(id)
         }
         const httpResponse = {
             headers: {
@@ -27,7 +30,10 @@ const createPathController = ((service: any) => {
     }
 })
 
-const pathController = createPathController(pathCacheService)
+const pathController = createPathController({
+    service: pathService,
+    cacheService: pathCacheService
+})
 
 export {
     pathController
--- subprojects/api/src/service/index.ts
+++ subprojects/api/src/service/index.ts
@@ -1,50 +1,30 @@
 import { fileDB } from '../repo/filedb'
 import { pathDB } from '../repo/typeorm'
+import { IPathDB } from '@clean/typings';
 
-import { root } from '../../config/paths'
-import * as path from 'path'
-import * as fs from 'fs'
-
-const createPathService = (db: any) => {
-    const getPath = async(id: string) => {
-        let result
-        try {
-            result = await db.getPath(id)
-        } catch(e) {
-            console.error('[!!!] production database is not running!, using cached filedb')
-            result = await fileDB.getPath(id)
-        }
+// persistence service
+const createPathService = (db: IPathDB) => {
+    const getPath = async(id: string) => 
         // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-        return result
-    }
+        await db.getPath(id)
     return {
         getPath
     }
 }
 
-// service
-const pathCacheService = (() => {
-    const getPath = async(id: string) => {
-        const data = fs.readFileSync(path.join(root, '../data/files.json'), 'utf-8')
-        let queryString = ''
-    
-        switch(id) {
-            case '0': 
-                queryString = 'queryRoot'
-                break
-        default:
-                queryString = `query${id}`
-                break
-        }
-        return await new Promise(res => setTimeout(res, 1000, (JSON.parse(data)[`${queryString}`])))
-    };
+// cached service
+const createPathCacheService = (db: IPathDB) => {
+    const getPath = async(id: string) => 
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        await db.getPath(id)
     return {
         getPath
     }
-})()
+}
 //
 
 const pathService = createPathService(pathDB)
+const pathCacheService = createPathCacheService(fileDB)
 
 export {
     pathService,

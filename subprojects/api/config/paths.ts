caf3aa813c20146b4d280f953a9be9663aa5529b
portfolio-y0711 <portfolio-y0711@gmail.com>
2021-03-18 15:17:04 +0900

    delete swagger static assets

--- subprojects/api/config/paths.ts
+++ subprojects/api/config/paths.ts
@@ -2,3 +2,4 @@ import path from "path"
 
 export const root: string = path.resolve(__dirname, "..")
 export const distroot: string = path.resolve(__dirname, "../../")
+export const distmoduleroot: string = path.resolve(__dirname, "../../../../node_modules")
--- subprojects/api/src/server/adaptors/index.ts
+++ subprojects/api/src/server/adaptors/index.ts
@@ -2,11 +2,11 @@ import { RegisterRoutes } from './routers/routes'
 import * as express from 'express'
 import * as path from 'path'
 import * as fs from 'fs';
-import { distroot } from '../../../config/paths';
+import { distroot, distmoduleroot } from '../../../config/paths';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const applyAdaptors = (app: express.Express) => {
-    app.use('/swagger-ui', express.static(path.join(distroot, './src/server/adaptors/swagger-ui')))
+    app.use('/swagger-ui', express.static(path.join(distmoduleroot, './swagger-ui/dist')))
     app.use('/v1/swagger.json', (_: unknown, res: express.Response) => {
         const json = fs.readFileSync(path.join(distroot, './swagger.json'), 'utf8')
         res.send(json)
